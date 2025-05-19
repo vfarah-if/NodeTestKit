76760216f7d5966ffda3dc6c6332444ba2613741
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-19 09:50:20 +0100

    (EDGE) Negative numbers test

--- add/add.should.test.ts
+++ add/add.should.test.ts
@@ -16,6 +16,7 @@ describe('add should', () => {
   test.each([
     { values: [1, 2], expected: 3 },
     { values: [1, 2, 3], expected: 6 },
+    { values: [-1, -2], expected: -3 },
   ])('sum $values to equal $expected', ({ values, expected }) => {
     const actual = add(...values);
     expect(actual).toBe(expected);
